2b5032ff8f3665ec41ae18e147028eef349956d5
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2025-03-08 19:15:29 +0700

    Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
@@ -30,16 +31,31 @@ class UserController extends Controller
         // UserModel::where('username', 'customer-1')->update($data);
 
         //fillable
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
 
-        UserModel::create($data);
+        // UserModel::create($data);
 
-        $user = UserModel::all();
+        //find
+        // $user = UserModel::find(1);
+
+        //firstWhere
+        // $user = UserModel::firstWhere('level_id', 1);
+
+
+        //findOr
+        // $user = UserModel::findOr(2, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        //findOrfail
+        // $user = UserModel::findOrfail(41);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
