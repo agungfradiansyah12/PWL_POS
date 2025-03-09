eceb2cdeba1b197e6f5809271eae118a5f7b037e
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2025-03-09 19:58:22 +0700

    Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,14 +5,14 @@
     <title>Data User</title>
 </head>
 <body>
-    <h1>Data User</h1>
+    {{-- <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspace="0">
         <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
-        </tr>
+        </tr> --}}
 {{-- @foreach ($data as $d)
         <tr>
             <td>{{$d->user_id}}</td>
@@ -21,13 +21,13 @@
             <td>{{$d->level_id}}</td>
         </tr>
 @endforeach --}}
-
+{{--
 <tr>
     <td>{{$data->user_id}}</td>
     <td>{{$data->username}}</td>
     <td>{{$data->nama}}</td>
     <td>{{$data->level_id}}</td>
-</tr>
+</tr> --}}
 
 {{-- <tr>
     <td>Jumlah Pengguna</td>
@@ -37,6 +37,26 @@
     <td>{{$data}}</td>
 </tr> --}}
 
+<h1>Data User</h1>
+<a href="user/tambah">+ Tambah User</a>
+<table border="1" cellpadding="2" cellspace="0">
+    <tr>
+        <th>ID</th>
+        <th>Username</th>
+        <th>Nama</th>
+        <th>ID Level Pengguna</th>
+        <th>Aksi</th>
+    </tr>
+    @foreach ($data as $d)
+    <tr>
+        <td>{{$d->user_id}}</td>
+        <td>{{$d->username}}</td>
+        <td>{{$d->nama}}</td>
+        <td>{{$d->level_id}}</td>
+        <td><a href="{{ url('/user/ubah/'.$d->user_id) }}">Ubah</a> |
+            <a href="{{ url('/user/hapus/'.$d->user_id) }}">Hapus</a></td>
+    </tr>
+    @endforeach
 
 </body>
 </html>
